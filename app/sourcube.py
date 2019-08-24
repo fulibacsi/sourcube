e12a57dcb4698366f36233e21b99f15b1f68803c
tami97 <tami19970811@gmail.com>
2019-08-24 15:08:01 +0200

    creating json file

--- app/sourcube.py
+++ app/sourcube.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 # regustering endpoint
 @app.route('/')
-def main():
+def index():
     return render_template('sourcube.html')
 
+@app.route('/generate', methods=['POST'])
+def generate():
+    return ''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
